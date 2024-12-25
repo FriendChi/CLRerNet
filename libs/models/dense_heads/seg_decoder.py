f72e1562c6aa5320030dbf667accb67967a770f5
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-25 17:23:13 +0800

    Update seg_decoder.py

--- libs/models/dense_heads/seg_decoder.py
+++ libs/models/dense_heads/seg_decoder.py
@@ -1,6 +1,65 @@
 import torch.nn.functional as F
 from torch import nn
 
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+class LKA(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 7, padding=7//2, groups=dim)
+        self.conv_spatial = nn.Conv2d(dim, dim, 9, stride=1, padding=((9//2)*4), groups=dim, dilation=4)
+        self.conv1 = nn.Conv2d(dim, dim, 1)
+        
+    def forward(self, x):
+        u = x.clone()        
+        attn = self.conv0(x)
+        attn = self.conv_spatial(attn)        
+        attn = self.conv1(attn)
+
+        return u * attn  
+
+class Attention(nn.Module):
+    def __init__(self, n_feats):
+        super().__init__()
+        
+        self.norm = LayerNorm(n_feats, data_format='channels_first')
+        self.scale = nn.Parameter(torch.zeros((1, n_feats, 1, 1)), requires_grad=True)
+        
+        self.proj_1 = nn.Conv2d(n_feats, n_feats, 1)
+        self.spatial_gating_unit = LKA(n_feats)
+        self.proj_2 = nn.Conv2d(n_feats, n_feats, 1)
+
+    def forward(self, x):
+        shorcut = x.clone()
+        x = self.proj_1(self.norm(x))
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        x = x*self.scale + shorcut
+        return x    
 
 class SegDecoder(nn.Module):
     """
@@ -22,9 +81,11 @@ class SegDecoder(nn.Module):
         self.conv = nn.Conv2d(prior_feat_channels * refine_layers, num_classes, 1)
         self.image_height = image_height
         self.image_width = image_width
+        self.mlka = Attention(prior_feat_channels * refine_layers)
 
     def forward(self, x):
         x = self.dropout(x)
+        x = self.mlka(x)
         x = self.conv(x)
         x = F.interpolate(
             x,

cf81f5429c1e58f12ac37f977b0430b0aa13c54e
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-25 18:45:40 +0800

    Update dla.py

--- libs/models/backbones/dla.py
+++ libs/models/backbones/dla.py
@@ -35,36 +35,6 @@ import torch.nn as nn
 import torch
 from torch.nn import functional as F
 
-class Channel_Att(nn.Module):
-    def __init__(self, channels):
-        super(Channel_Att, self).__init__()
-        self.channels = channels
-
-        self.bn2 = nn.BatchNorm2d(self.channels, affine=True)
-
-    def forward(self, x):
-        residual = x
-
-        x = self.bn2(x)
-        weight_bn = self.bn2.weight.data.abs() / torch.sum(self.bn2.weight.data.abs())
-        x = x.permute(0, 2, 3, 1).contiguous()
-        x = torch.mul(weight_bn, x)
-        x = x.permute(0, 3, 1, 2).contiguous()
-
-        x = torch.sigmoid(x) * residual  #
-
-        return x
-
-
-class NAMAttention(nn.Module):
-    def __init__(self, channels):
-        super(NAMAttention, self).__init__()
-        self.Channel_Att = Channel_Att(channels)
-
-    def forward(self, x):
-        x_out1 = self.Channel_Att(x)
-
-        return x_out1
         
 class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, dilation=1):
@@ -89,7 +59,6 @@ class BasicBlock(nn.Module):
             bias=False,
             dilation=dilation,
         )
-        self.nam = NAMAttention(planes)
         self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
         self.stride = stride
 
@@ -103,7 +72,6 @@ class BasicBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.nam(out)
         out += residual
         out = self.relu(out)
 

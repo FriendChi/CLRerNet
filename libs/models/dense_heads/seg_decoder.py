472f393bd1f5a5886d211fe64d91532c18b87c0c
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-26 10:57:04 +0800

    Update seg_decoder.py

--- libs/models/dense_heads/seg_decoder.py
+++ libs/models/dense_heads/seg_decoder.py
@@ -1,29 +1,28 @@
 import torch.nn.functional as F
 from torch import nn
 
-class LKAT(nn.Module):
-    def __init__(self, n_feats):
-        super().__init__()
-       
-        #self.norm = LayerNorm(n_feats, data_format='channels_first')
-        #self.scale = nn.Parameter(torch.zeros((1, n_feats, 1, 1)), requires_grad=True)
-        
-        self.conv0 = nn.Sequential(
-            nn.Conv2d(n_feats, n_feats, 1, 1, 0),
-            nn.GELU())
-        
-        self.att = nn.Sequential(
-                nn.Conv2d(n_feats, n_feats, 7, 1, 7//2, groups= n_feats),  
-                nn.Conv2d(n_feats, n_feats, 9, stride=1, padding=(9//2)*3, groups=n_feats, dilation=3),
-                nn.Conv2d(n_feats, n_feats, 1, 1, 0))  
-
-        self.conv1 = nn.Conv2d(n_feats, n_feats, 1, 1, 0)
-        
+class SE_Block(nn.Module):
+    def __init__(self, inchannel, ratio=16):
+        super(SE_Block, self).__init__()
+        # 全局平均池化(Fsq操作)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        # 两个全连接层(Fex操作)
+        self.fc = nn.Sequential(
+            nn.Linear(inchannel, inchannel // ratio, bias=False),  # 从 c -> c/r
+            nn.ReLU(),
+            nn.Linear(inchannel // ratio, inchannel, bias=False),  # 从 c/r -> c
+            nn.Sigmoid()
+        )
+ 
     def forward(self, x):
-        x = self.conv0(x)
-        x = x*self.att(x) 
-        x = self.conv1(x) 
-        return x    
+            # 读取批数据图片数量及通道数
+            b, c, h, w = x.size()
+            # Fsq操作：经池化后输出b*c的矩阵
+            y = self.gap(x).view(b, c)
+            # Fex操作：经全连接层输出（b，c，1，1）矩阵
+            y = self.fc(y).view(b, c, 1, 1)
+            # Fscale操作：将得到的权重乘以原来的特征图x
+            return x * y.expand_as(x)
 
 class SegDecoder(nn.Module):
     """
@@ -45,7 +44,7 @@ class SegDecoder(nn.Module):
         self.conv = nn.Conv2d(prior_feat_channels * refine_layers, num_classes, 1)
         self.image_height = image_height
         self.image_width = image_width
-        self.mlka = LKAT(prior_feat_channels * refine_layers)
+        self.mlka = SE_Block(prior_feat_channels * refine_layers)
 
     def forward(self, x):
         x = self.dropout(x)
